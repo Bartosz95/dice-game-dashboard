5af1e86d1cac3c068b836641bf80e8c05cdd11c2
Bartosz Lenartowicz <bartosz.lenartowicz@nokia.com>
2020-10-08 15:42:52 +0200

    delete host

--- src/app/services/game-client.service.ts
+++ src/app/services/game-client.service.ts
@@ -12,7 +12,7 @@ export class GameClientService {
 
   }
 
-  rootURL = 'http://localhost:3000/api/v1/game';
+  rootURL = '/api/v1/game';
 
   public getGame(): Observable<Game> {
     return this.httpClient.get<Game>(this.rootURL);
